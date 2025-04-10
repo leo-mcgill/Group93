77f0f216f0ebdfc322f81fd1a5f9dcf01c2379e4
leo-mcgill <leomcgill1@icloud.com>
2025-04-10 11:49:42 +0800

    Created base.html that only contains navbar, all other templates extend from base.html to prevent us from rewriting navbar code for every template. Added some login/signup routes in app.py and also a login/signup page (currently non-functional as DB is not initialised yet). Will get DB up and running so we can add users and add movies

--- README.md
+++
@@ -1,2 +0,0 @@
-# Group93
-Agile Web Development project created by Group 93
--- app/app.py
+++ app/app.py
@@ -1,6 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User, Movie
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdb.sqlite3'
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Logged in successfully.', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid credentials.', 'danger')
+    return render_template("login.html")
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        flash('Username already exists.', 'warning')
+        return redirect(url_for('login'))
+
+    new_user = User(username=username)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+    flash('Account created successfully. You can now log in.', 'success')
+    return redirect(url_for('login'))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out successfully.", "info")
+    return redirect(url_for("index"))
 
 @app.route('/')
 def home():
