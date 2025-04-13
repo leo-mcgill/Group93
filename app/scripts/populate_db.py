683efc3313bbb9f3769ed1e134b948f60473b4a0
leo-mcgill <leomcgill1@icloud.com>
2025-04-13 15:36:15 +0800

    small adjustment to the script to populate the DB as I dont have a need to populate the Db with a movie just yet. had an issue with the requirements.txt but it should be solved now

--- app/scripts/populate_db.py
+++ app/scripts/populate_db.py
@@ -6,13 +6,15 @@ from app import app, db
 from models import User, Movie
 
 with app.app_context():
+    #Just creating user for now
     user = User(username="testuser")
     user.set_password("password123")
     db.session.add(user)
     db.session.commit()
     
-    movie = Movie(title="Up", user_id=user.id)
-    db.session.add(movie)
-    db.session.commit()
+    
+    #movie = Movie(title="Up", user_id=user.id)
+    #db.session.add(movie)
+    #db.session.commit()
     
     print("Test data added.")
