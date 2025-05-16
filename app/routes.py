ab04e62442b1792ca2ab3fbfe2d8a9b7dee65019
khailing <angkl0709@gmail.com>
2025-05-16 22:17:58 +0800

    backend validation of confirm password added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 #Group 93 CITS3403 Project 2025
 #routes file for all routes used by flask app
 
-from flask import Flask, current_app, render_template, redirect, url_for, request, flash, jsonify
+from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User, Movie, UserMovie
 from sqlalchemy.orm import aliased
@@ -56,6 +56,11 @@ def init_routes(application):
         if request.method == 'POST':
             username = form.username.data
             password = form.password.data
+            confirmPassword = request.form.get('confirm_password')
+
+            if password != confirmPassword:
+                flash("Password do not match.Please try again..", 'warning')
+                return jsonify({'message':'Password do not match'})
             
             existing_user = User.query.filter_by(username=username).first()
             if existing_user:
@@ -91,7 +96,7 @@ def init_routes(application):
 
         movies = Movie.query.all()
         
-        return render_template("uploadReview.html", api_key=Config.API_KEY, underlined_tab_index=2, movies = movies, use_omdb_api=Config.USE_OMDB_API)
+        return render_template("uploadReview.html", api_key=Config.API_KEY, underlined_tab_index=2, movies = movies)
     
 
 
@@ -107,20 +112,11 @@ def init_routes(application):
             return int(val) if val not in [None, "N/A"] else default
         except:
             return default
-    
-    ### ROUTE TO UPLOAD MOVIE - DEPENDING ON CONFIG
+        
+    ### ROUTE TO MAKE THE OMDB REQUEST, AND STORE THE RESPONSE IN THE DB ###
     @application.route('/upload_movie', methods=['POST'])
     @login_required
     def upload_movie():
-        if current_app.config['USE_OMDB_API']:
-            return upload_omdb_movie()
-        else:
-            return upload_existing_movie()
-    
-    ### ROUTE TO MAKE REQUEST SAMPLE MOVIE FROM DB, AND STORE THE RESPONSE IN THE DB ###
-    @application.route('/upload_existing_movie', methods=['POST'])
-    @login_required
-    def upload_existing_movie():
         try:
             data = request.get_json()
             title = data.get('movie_title')
@@ -185,84 +181,6 @@ def init_routes(application):
         # Return a list of matching movies
         return jsonify({"results": [movie.title for movie in matching_movies]})
 
-    @application.route('/upload_omdb_movie', methods=['POST'])
-    @login_required
-    def upload_omdb_movie():
-        try:
-            data = request.get_json()
-            title = data.get('movie_title')
-            user_rating = data.get('user_rating')
-            
-            # These lines of code add new movies to the database using the OMDB API Key.
-            response = requests.get(f"https://www.omdbapi.com/?t={title}&apikey={Config.API_KEY}")
-            movie_data = response.json()
-
-            if movie_data.get("Response") == "False":
-                return jsonify({"error": f"Movie: {title} not found"}), 404
-
-            # Safely extract fields
-            runtime_str = movie_data.get("Runtime", "0").replace(" min", "")
-            runtime = safe_int(runtime_str, default=0)
-
-            imdb_rating = safe_float(movie_data.get("imdbRating"))
-            metascore = safe_int(movie_data.get("Metascore"))
-            rt_rating = (
-                movie_data.get("Ratings", [{}])[1].get("Value")
-                if len(movie_data.get("Ratings", [])) > 1
-                else None
-            )
-            
-            
-            movie = Movie.query.filter_by(
-                title=movie_data.get("Title", "Unknown"),
-                year=movie_data.get("Year")).first()
-            
-            
-            if not movie:
-                movie = Movie(
-                    title=movie_data.get("Title", "Unknown"),
-                    year=movie_data.get("Year"),
-                    rated=movie_data.get("Rated"),
-                    released=movie_data.get("Released"),
-                    runtime=movie_data.get("Runtime"),
-                    genre=movie_data.get("Genre"),
-                    director=movie_data.get("Director"),
-                    writer=movie_data.get("Writer"),
-                    actors=movie_data.get("Actors", "Unknown"),
-                    language=movie_data.get("Language"),
-                    country=movie_data.get("Country"),
-                    imdb_rating=movie_data.get("imdbRating"),
-                    rt_rating=rt_rating,
-                    metascore=movie_data.get("Metascore"),
-                    box_office=movie_data.get("BoxOffice"),
-                    poster_url=movie_data.get("Poster")
-                )
-                db.session.add(movie)
-                db.session.commit()
-            existing = Movie.query.filter_by(title=data.get('movie_title')).first()
-            existing_link = UserMovie.query.filter_by(user_id=current_user.id, movie_id=existing.id).first()
-
-            if existing_link:
-                existing_link.user_rating = user_rating
-                message = f"Movie: {title} rating updated!"
-            else:
-                # Change existing to movie when you want to add movies to db using OMDB API Key
-                user_movie = UserMovie(user_id=current_user.id, movie_id=movie.id, user_rating=user_rating)
-                db.session.add(user_movie)
-                message = f"Movie: {title} added to your list!"
-            
-            
-            db.session.commit()
-            return jsonify({"message": message}), 200
-
-        except Exception as e:
-            import traceback
-            traceback.print_exc()
-            return jsonify({"error": f"Failed to store movie: {str(e)}"}), 500
-
-
-    
-        
     @application.route('/shareData')
     @login_required
     def shareData():
@@ -640,10 +558,6 @@ def init_routes(application):
         # Get the username in the query parameters
         username = request.args.get('username')
 
-        current_username = User.query.get(current_user.username)
-        if current_username != username:
-            return jsonify({"error": "Current user is not the requested user"}), 404
-
         # If the username is not provided or the username is currently logged in, the current user's profile will be displayed.
         if not username or username == current_user.username:
             profile_user = current_user
@@ -810,3 +724,4 @@ def init_routes(application):
         db.session.commit()
         flash(f"Rating for “{title}” deleted.", "success")
         return redirect(url_for('manage_ratings'))
+
